9f0e25c258f3ec09e0eb751b4428bb3f4d665608
sanya152 <fonarev.sanya@yandex.ru>
2023-09-06 14:26:29 +0300

    init

--- src/main/java/modul_1/z1.java
+++
@@ -1,7 +0,0 @@
-package modul_1;
-
-public class z1 {
-    public static void main(String[] args) {
-        System.out.println("hello word");
-    }
-}
---
+++ src/main/java/modul_1/z_1_2_4.java
@@ -0,0 +1,16 @@
+package modul_1;
+
+public class z_1_2_4 {
+    public static void main(String[] args) {
+        byte varByte = 10;
+        short varShort = 20;
+        int varInt = 30;
+        long varLong = 40;
+        char varchar = 50;
+        float varFloat = 60;
+        double varDouble = 70;
+        boolean h;
+
+
+    }
+}
