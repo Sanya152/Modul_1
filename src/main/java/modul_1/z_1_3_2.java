83c0aded790af6ebb0db782e881c6987d43a8b5a
sanya152 <fonarev.sanya@yandex.ru>
2023-09-09 08:56:36 +0300

    init 1.3.2

--- src/main/java/modul_1/z_1_3_2.java
+++ src/main/java/modul_1/z_1_3_2.java
@@ -2,7 +2,7 @@ package modul_1;
 
 public class z_1_3_2 {
     public static void main(String[] args) {
-        System.out.println(charExpression(10956));
+        System.out.println(charExpression(1050));
     }
     public static char charExpression(int a) {
         char x ='\\';
