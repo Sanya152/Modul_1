1e24d33e0b0d8a86f0c245fa06908805ff13427d
sanya152 <fonarev.sanya@yandex.ru>
2023-09-09 09:40:33 +0300

    init 1.3.3

---
+++ src/main/java/modul_1/z_1_3_3.java
@@ -0,0 +1,10 @@
+package modul_1;
+
+public class z_1_3_3 {
+    public static void main(String[] args) {
+        System.out.println(getAgeDiff((byte) 4, (byte) 3));
+    }
+    public static byte getAgeDiff(byte age1, byte age2) {
+        return (byte) Math.abs(age1 - age2);
+    }
+}
