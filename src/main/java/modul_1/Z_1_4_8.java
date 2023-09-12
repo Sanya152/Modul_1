795288df366fa55e160fb17ed5e94fe4d3728a1b
alexandr <fonarev.sanya@yandex.ru>
2023-09-12 22:20:50 +0300

    init Z_1_4_8

---
+++ src/main/java/modul_1/Z_1_4_8.java
@@ -0,0 +1,16 @@
+package modul_1;
+
+import java.math.BigInteger;
+
+public class Z_1_4_8 {
+    public static void main(String[] args) {
+        System.out.println(factorial(6));
+    }
+    public static BigInteger factorial(int value) {
+        if (value <= 1) {
+            return BigInteger.valueOf(1);
+        }else{
+            return BigInteger.valueOf(value).multiply(factorial(value-1));
+        }
+    }
+}
